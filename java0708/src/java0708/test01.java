56032e01ff24ca90ce714b7f4b0447f8fbc274ae
hyeong <tpgud3331@gmail.com>
2024-07-08 16:24:45 +0900

    Scanner 사용 예제

---
+++ java0708/src/java0708/test01.java
@@ -0,0 +1,17 @@
+package java0708;
+import java.util.Scanner;
+public class test01 {
+	public static void main(String[] arg) {
+		Scanner scanner = new Scanner(System.in);
+		String a1,a2,a3;
+		
+		
+			a1=scanner.nextLine();
+			a2=scanner.next();
+			a3=scanner.next();
+			
+			
+	
+		System.out.println("a1"+a1+"a2"+a2+"a3"+a3);
+	}
+}
